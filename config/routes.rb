a59046ba518fa1e98e10f1b42348fd721645818a
Mariya Rose <mariel.jesm@gmail.com>
2021-08-31 10:22:43 +1000

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :create, :destroy]
+  resources :users, except: [:destroy] do
+    resources :bookings
   end
-  resources :pets, only: [:index, :show, :new, :create, :destroy] do
-    resources :bookings, only: [:new, :create, :destroy]
+  resources :pets do
+    resources :bookings
   end
 end
