cc85b672b2470723a7a0cdbd56ea0d0467ff0e8f
Mariya Rose <mariel.jesm@gmail.com>
2021-08-31 11:41:52 +1000

    Typo Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, except: [:destroy] do
-    resources :bookings
-  end
+  resources :users except: [:destroy]
   resources :pets do
     resources :bookings
   end
+  root to: "pages#home"
 end
