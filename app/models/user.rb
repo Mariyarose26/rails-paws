bf0fa804e2c1d5f7940b70002e5fdb96cea61bfa
Mariya Rose <mariel.jesm@gmail.com>
2021-08-31 17:44:59 +1000

    Profile pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings, dependent: :destroy
   has_many :pets, through: :bookings
+  has_one_attached :photo
   validates :first_name, :last_name, :address, :email, :password, presence: true
   validates :password, length: { minimum: 6 }
   validates :password, confirmation: true
