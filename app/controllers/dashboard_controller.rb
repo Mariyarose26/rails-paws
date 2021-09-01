d4d5f24066b73b845114511907e94be99376398f
Mariya Rose <mariel.jesm@gmail.com>
2021-09-01 16:10:55 +1000

    Navbar changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,9 @@ class DashboardController < ApplicationController
   def dashboard
     @user = current_user
     @pets = current_user.pets
+    # Owner
     @bookings = Booking.where(pet: @pets)
+    # Sitter
     @current_user_bookings = Booking.where(user: current_user)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :address)
+    params.require(:user).permit(:first_name, :last_name, :email, :address, :user_type)
   end
 
   def find_user
