b6c8db6b07e4984ff377e6ab881f73bad6da01f2
Mariya Rose <mariel.jesm@gmail.com>
2021-09-01 23:11:51 +1000

    Tabs Working

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,8 @@ class DashboardController < ApplicationController
     @user = current_user
     @pets = current_user.pets
     # Owner
-    @bookings = Booking.where(pet: @pets)
+    @ownerbookings = Booking.where(pet: @pets)
     # Sitter
-    @current_user_bookings = Booking.where(user: current_user)
+    @bookings = Booking.where(user: @user)
   end
 end
