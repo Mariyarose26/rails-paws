9cae0d11a45542bbff58f99f455bbad576d7cf28
Mariya Rose <mariel.jesm@gmail.com>
2021-09-01 17:28:34 +1000

    Password field removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :pets
   has_one_attached :photo
-  validates :first_name, :last_name, :address, :email, :password, presence: true
-  validates :password, length: { minimum: 6 }
-  validates :password, confirmation: true
+  validates :first_name, :last_name, :address, :email, :user_type, presence: true
+  # validates :password, length: { minimum: 6 }
+  # validates :password, confirmation: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum user_type: [:owner, :sitter, :both]
