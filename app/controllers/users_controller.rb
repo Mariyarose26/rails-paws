49b4db139de61000b39ef45cd3aaeceef4e3dac5
Mariya Rose <mariel.jesm@gmail.com>
2021-09-02 23:42:25 +1000

    Add Sitter link to dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :address, :user_type, :photo)
+    params.require(:user).permit(:first_name, :last_name, :email, :address, :user_type, :photo, :profile)
   end
 
   def find_user
